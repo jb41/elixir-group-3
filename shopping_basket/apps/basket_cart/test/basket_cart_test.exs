22d54950ee82eab4382ac9e4d34f784ab8595b4a
Graham Hay <grahamrhay@gmail.com>
2016-04-30 14:01:18 +0100

    Implemented basket

--- shopping_basket/apps/basket_cart/test/basket_cart_test.exs
+++ shopping_basket/apps/basket_cart/test/basket_cart_test.exs
@@ -13,4 +13,23 @@ defmodule BasketCartTest do
     assert BasketCart.count(new_cart) == 1
     assert BasketCart.current_items(new_cart) == ["Banana"]
   end
+
+  test "Add another item to basket" do
+    cart = BasketCart.new
+    cart2 = BasketCart.add(cart, "Banana")
+    cart3 = BasketCart.add(cart2, "Banana")
+
+    assert BasketCart.count(cart3) == 2
+    assert BasketCart.current_items(cart3) == ["Banana", "Banana"]
+  end
+
+  test "Remove an item from basket" do
+    cart = BasketCart.new
+    cart = BasketCart.add(cart, "Banana")
+    cart = BasketCart.add(cart, "Orange")
+    cart = BasketCart.remove(cart, "Banana")
+
+    assert BasketCart.count(cart) == 1
+    assert BasketCart.current_items(cart) == ["Orange"]
+  end
 end
